8ee28a338f5e4d0bf3a6a65409eaa7f9690dc47a
chrisyuen976 <chrisyuen976@gmail.com>
2022-09-26 22:19:33 +1000

    Styling

--- src/Character-select.js
+++ src/Character-select.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
-function CharacterSelect({ bgClass }) {
+function CharacterSelect({ bgClass, props }) {
   return (
-    <div className={`rhombus-container ${bgClass}`}>
+    <div className={`d-flex rhombus-container ${bgClass}`}>
       <div class="rhombus-content">
-        <h1>Name</h1>
-        <h1>Image</h1>
-        <h2>Developer</h2>
+        <h2>{props.name}</h2>
+        <img src={props.imageUrl} alt={props.name + "image"} class="mb-3"/>
+        <h2 class="role_text">{props.role}</h2>
       </div>
     </div>
   );
